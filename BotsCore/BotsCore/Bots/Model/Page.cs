70d0298d5c79e319bf8da215d58e7ba209c8645f
Bocmen <den20025plus@gmail.com>
2021-03-07 14:38:05 +0300

    Изменены стандартные методы страниц

---
+++ BotsCore/BotsCore/Bots/Model/Page.cs
@@ -0,0 +1,32 @@
+﻿using BotsCore.Bots.Model.Buttons;
+
+namespace BotsCore.Bots.Model
+{
+    public abstract class Page
+    {
+        /// <summary>
+        /// Событие установки страницы пользователю (первое открытие)
+        /// </summary>
+        public virtual void Open(ObjectDataMessageInBot inBot) { }
+        /// <summary>
+        /// Событие загрузки страницы из хранилища (как правило вызывается после перезагрузки бота)
+        /// </summary>
+        public virtual void StoreLoad(ObjectDataMessageInBot inBot) { }
+        /// <summary>
+        /// Событие перед закрытием страницы
+        /// </summary>
+        public virtual void Close(ObjectDataMessageInBot inBot) { }
+        /// <summary>
+        /// Событие обработки входящих сообщений
+        /// </summary>
+        public abstract void InMessage(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Получение набора кнопок клавиатуры для данной страницы
+        /// </summary>
+        public virtual KitButton GetKeyboardButtons(ObjectDataMessageInBot inBot) => null;
+        /// <summary>
+        /// Повторный запрос отправки последнего сообщения
+        /// </summary>
+        public abstract void ResetLastMessenge(ObjectDataMessageInBot inBot);
+    }
+}
