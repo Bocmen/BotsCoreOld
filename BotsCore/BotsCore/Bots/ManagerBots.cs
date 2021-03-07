fdc0aee8c2a74c84b53d656387a594a82038c368
Bocmen <den20025plus@gmail.com>
2021-03-07 14:43:47 +0300

    Переделана отправка сообщений

--- BotsCore/BotsCore/Bots/ManagerBots.cs
+++ BotsCore/BotsCore/Bots/ManagerBots.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using BotsCore.Bots.Model;
 using System.Linq;
+using BotsCore.User.Models;
+using System;
+using BotsCore.Moduls;
 
 namespace BotsCore.Bots
 {
     public static class ManagerBots
     {
-        private static Dictionary<string, (IBot bot, bool isStarted)> botsList = new Dictionary<string, (IBot bot, bool isStarted)>();
-        public static void AddBot(IBot bot) => botsList.Add(GetNameBot(bot), (bot, false));
+        private static Dictionary<string, IBot> botsList = new Dictionary<string, IBot>();
+        public static void AddBot(IBot bot) => botsList.Add(GetNameBot(bot), bot);
         public static string GetNameBot(IBot bot) => $"N={bot.GetId()}T={bot.GetBotTypes()}";
-        public static void StartBots() => botsList.AsParallel().ForAll(x => { if (!x.Value.isStarted) { x.Value.bot.Start(); botsList[x.Key] = (x.Value.bot, true); } });
-        public static void SendDataBot(ObjectDataMessageSend messageSend)
+        public static bool SendDataBot(ObjectDataMessageSend messageSend)
         {
-            messageSend.inBot.botHendler.SendDataBot(messageSend);
-            messageSend.inBot.User.LoadToDataBD();
+            try
+            {
+                if (messageSend.inBot.botHendler != default)
+                {
+                    messageSend.inBot.botHendler.SendDataBot(messageSend);
+                    return true;
+                }
+                IBot bot = GetBot(messageSend.inBot.botID.BotKey);
+                if (bot != default)
+                {
+                    bot.SendDataBot(messageSend);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                EchoLog.Print($"Не удалось выполнить отправку сообщений. [{e.Message}]", EchoLog.PrivilegeLog.Warning);
+            }
+            return false;
+        }
+        public static IBot GetBot(string key)
+        {
+            if (botsList.TryGetValue(key, out IBot resul))
+                return resul;
+            return default;
         }
     }
 }
