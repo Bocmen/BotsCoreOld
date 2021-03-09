6fdc4da564094fa66fa6cb969af4a867d2da3bbd
Bocmen <den20025plus@gmail.com>
2021-03-09 23:43:35 +0300

    Изменены входные параметры методов

--- BotsCore/BotsCore/Bots/Interface/ISettingManagerPage.cs
+++ BotsCore/BotsCore/Bots/Interface/ISettingManagerPage.cs
@@ -1,19 +1,39 @@
 ﻿using BotsCore.Bots.Model;
 using BotsCore.Bots.Model.Buttons;
 using BotsCore.Bots.Model.Buttons.Command;
-using BotsCore.Moduls.Translate;
 using System;
 
 namespace BotsCore.Bots.Interface
 {
     public interface ISettingManagerPage
     {
-        public Button[][] GetStandartButtons();
-        public bool SetStandartButtonsCreteUser();
-        public string GetTextCreteUser(Lang.LangTypes lang);
-        public (string NameApp, string NamePage) GetPageCreteUser();
-        public string GetTextSetButtons(Lang.LangTypes lang);
-        public CommandList GetSpecialCommand();
-        public Action<ObjectDataMessageInBot> GetRegisterMethod();
+        /// <summary>
+        /// Стандартный набор кнопок который выдаётся по умолчанию
+        /// </summary>
+        public Button[][] GetStandartButtons(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Выдать ли стандартный набор кнопок при создании пользователя
+        /// </summary>
+        public bool SetStandartButtonsCreteUser(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Текст выводимый пользователю при его первому взаимодействию с платформой
+        /// </summary>
+        public string GetTextCreteUser(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Первая страница после создание пользователя
+        /// </summary>
+        public (string NameApp, string NamePage) GetPageCreteUser(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Стандартный текст при выдаче кнопок (если текст не был указан
+        /// </summary>
+        public string GetTextSetButtons(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Набор команд которые будут проверяться первыми (перед отправкой сообщения на обработку странице)
+        /// </summary>
+        public CommandList GetSpecialCommand(ObjectDataMessageInBot inBot);
+        /// <summary>
+        /// Получения метода обрабатывающего регистрацию пользователя
+        /// </summary>
+        public Action<ObjectDataMessageInBot> GetRegisterMethod(ObjectDataMessageInBot inBot);
     }
 }
