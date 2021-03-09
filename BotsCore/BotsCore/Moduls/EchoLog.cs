62ef6107c0123bbf77796bec08884211cf4b7e58
Bocmen <den20025plus@gmail.com>
2021-03-09 18:35:29 +0300

    Добавлена возможность отключения логирования

--- BotsCore/BotsCore/Moduls/EchoLog.cs
+++ BotsCore/BotsCore/Moduls/EchoLog.cs
@@ -7,6 +7,10 @@ namespace BotsCore.Moduls
     /// </summary>
     public static class EchoLog
     {
+#pragma warning disable CA2211 // Поля, не являющиеся константами, не должны быть видимыми
+        public static bool IsConsoleLog = true;
+#pragma warning restore CA2211 // Поля, не являющиеся константами, не должны быть видимыми
+
         /// <summary>
         /// Залогировать событие
         /// </summary>
@@ -14,29 +18,36 @@ namespace BotsCore.Moduls
         /// <param name="privilege">Приоритет события</param>
         public static void Print(string TextLog, string Path, PrivilegeLog privilege = PrivilegeLog.Info)
         {
-            try
+            if (IsConsoleLog)
             {
-                switch (privilege)
+                try
                 {
-                    case PrivilegeLog.Error:
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        break;
-                    case PrivilegeLog.Info:
-                        Console.ForegroundColor = ConsoleColor.White;
-                        break;
-                    case PrivilegeLog.LogSystem:
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        break;
-                    case PrivilegeLog.Warning:
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        break;
+                    switch (privilege)
+                    {
+                        case PrivilegeLog.Error:
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            break;
+                        case PrivilegeLog.Info:
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                        case PrivilegeLog.LogSystem:
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                            break;
+                        case PrivilegeLog.Warning:
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            break;
+                    }
+                    Console.WriteLine($"[{Path}] [{DateTime.Now.ToLocalTime()} {privilege}] {TextLog}");
+                    Console.ResetColor();
                 }
-                Console.WriteLine($"[{Path}] [{DateTime.Now.ToLocalTime()} {privilege}] {TextLog}");
-                Console.ResetColor();
+                catch { }
             }
-            catch { }
         }
-        public static void Print(string TextLog, PrivilegeLog privilege = PrivilegeLog.Info) => Print(TextLog, new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().DeclaringType.FullName.ToString(), privilege);
+        public static void Print(string TextLog, PrivilegeLog privilege = PrivilegeLog.Info)
+        {
+            if (IsConsoleLog)
+                Print(TextLog, new System.Diagnostics.StackTrace().GetFrame(1).GetMethod().DeclaringType.FullName.ToString(), privilege);
+        }
         /// <summary>
         /// Виды привелегий
         /// </summary>
